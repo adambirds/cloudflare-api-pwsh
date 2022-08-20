6fea1ea5066d76d68a4337a6ab112733ce85d5be
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2022-08-20 03:09:39 +0100

    Added API url in module file.

--- PWSHCloudFlareAPI/PWSHCloudFlareAPI.psm1
+++ PWSHCloudFlareAPI/PWSHCloudFlareAPI.psm1
@@ -1,3 +1,5 @@
+$Script:APIUri = "https://api.cloudflare.com/client/v4/"
+
 #Dot source all functions in all ps1 files located in the module's public and private folders, excluding tests and profiles.
 Get-ChildItem -Path $PSScriptRoot\Public\*.ps1, $PSScriptRoot\Private\*.ps1 -Exclude *.tests.ps1, *profile.ps1 -ErrorAction SilentlyContinue |
 ForEach-Object {
